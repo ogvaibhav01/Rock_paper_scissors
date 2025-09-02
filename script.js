773c5b6bd092c0c4364dbca6283cead05a790286
Vaibhav Bhardwaj <vaibhavbhardwaj3010@gmail.com>
2025-09-02 18:42:28 +0530

    fixed UI

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 // Rock paper scissors game logic
-const choices = ['rock', 'paper', 'scissors'];
+    function play(userChoice){
+    const choices = ['rock', 'paper', 'scissors'];
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+
+    let result = '';
+
+    if (userChoice === computerChoice) {
+      result = "It's a tie!";
+    } else if (
+      (userChoice === 'rock' && computerChoice === 'scissors') ||
+      (userChoice === 'paper' && computerChoice === 'rock') ||
+      (userChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+      result = "You win!";
+    } else {
+      result = "You lose!";
+    }
+
+    alert(`You chose ${userChoice}, computer chose ${computerChoice}. ${result}`);
+}
 
 
